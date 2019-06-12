2695d7d8abd8a4a37f4d69f8fd5d6dd3790bd34f
JoeEdwardsGitHub <48823574+JoeEdwardsGitHub@users.noreply.github.com>
2019-06-12 12:52:17 +0100

    Examples of reading and writing to data lake

---
+++ notebooks/ReadWriteTest.py
@@ -0,0 +1,110 @@
+# Databricks notebook source
+# Authenticate to Azure data lake using Azure key vault
+spark.conf.set(
+  "fs.azure.account.key.tescohackathon.dfs.core.windows.net",
+  dbutils.secrets.get(scope = "AzureKeyVault", key = "StorageKey")
+)
+
+# COMMAND ----------
+
+# Read data from data lake into Spark dataframe, 
+inputdata = spark.read.csv("abfss://input@tescohackathon.dfs.core.windows.net/test.csv")
+
+# COMMAND ----------
+
+# Convert to delta lake parquet format for efficient SQL table storage
+inputdata.write.format("delta").save("/delta/inputdata")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- Write delta lake parquet to a table
+# MAGIC DROP TABLE IF EXISTS inputdata;
+# MAGIC 
+# MAGIC CREATE TABLE inputdata USING DELTA LOCATION '/delta/inputdata/'
+
+# COMMAND ----------
+
+# Write Spark dataframe to data lake
+inputdata.write.csv("abfss://output@tescohackathon.dfs.core.windows.net/data", "overwrite")
+
+# COMMAND ----------
+
+# Import Python libraries
+import numpy as np
+import pandas as pd
+import seaborn as sb
+
+# COMMAND ----------
+
+# Enable Arrow-based columnar data transfers
+spark.conf.set("spark.sql.execution.arrow.enabled", "true")
+
+# COMMAND ----------
+
+# Generate a Pandas DataFrame
+pdf = pd.DataFrame(np.random.rand(100, 3))
+
+# Create a Spark DataFrame from a Pandas DataFrame using Arrow
+df = spark.createDataFrame(pdf, ['a', 'b', 'c'])
+
+# Convert the Spark DataFrame back to a Pandas DataFrame using Arrow
+result_pdf = df.select("*").toPandas()
+
+# COMMAND ----------
+
+pdf.head()
+
+# COMMAND ----------
+
+df.head()
+
+# COMMAND ----------
+
+result_pdf.head()
+
+# COMMAND ----------
+
+df.write.csv("abfss://output@tescohackathon.dfs.core.windows.net/pandasdata", "overwrite")
+
+# COMMAND ----------
+
+print(result_pdf['a'].value_counts)
+print(result_pdf['a'].mean())
+
+# COMMAND ----------
+
+ax = sb.countplot(x='a', data=result_pdf)
+
+# COMMAND ----------
+
+ax
+display()
+
+# COMMAND ----------
+
+pdf
+
+# COMMAND ----------
+
+df
+
+# COMMAND ----------
+
+pandasinputdata = inputdata.select("*").toPandas()
+
+# COMMAND ----------
+
+pandasinputdata
+
+# COMMAND ----------
+
+pandasinputdata['Col3']=1
+
+# COMMAND ----------
+
+pandasinputdata
+
+# COMMAND ----------
+
+
